47d2f084d2eff544a9b4702cdd7d6ec10a1d00ab
Vu Bui Khanh Linh <s3864120@rmit.edu.vn>
2022-03-24 00:46:07 +0700

    Add logic for running functions in main()

--- src/Enrollment/StudentEnrolmentList.java
+++ src/Enrollment/StudentEnrolmentList.java
@@ -1,15 +1,42 @@
 package Enrollment;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class StudentEnrolmentList implements StudentEnrolmentManager {
 
     ArrayList<StudentEnrolment> enrollList;
+    HashSet<Course> courseList = allCourses();
 
     @Override
     /* Add new enrolment */
-    public void addEnrolment(String sid, String name, String date, String cid, String cname, int credit_num, String semester) {
-        Student student = new Student(sid, name, date);
+    public void addEnrolment(String sidOrName, String cidOrName, String semester) {
+        String sid = "";
+        String sname = "";
+        String date = "";
+        String cid = "";
+        String cname = "";
+        int credit_num = 0;
+        for (StudentEnrolment s : enrollList) {
+            if (s.getStudent().getId() == sidOrName || s.getStudent().getName() == sidOrName) {
+                //Get studentInfo
+                sid = s.getStudent().getId();
+                sname = s.getStudent().getName();
+                date = s.getStudent().getBirthday();
+                break;
+            }
+        }
+        for (Course c : courseList) {
+            if (c.getId() == cidOrName || c.getName() == cidOrName) {
+                //Get courseInfo
+                cid = c.getId();
+                cname = c.getName();
+                credit_num = c.getCredit_num();
+                break;
+            }
+        }
+
+        Student student = new Student(sid, sname, date);
         Course course = new Course(cid, cname, credit_num);
         StudentEnrolment newEnrolment = new StudentEnrolment(student, course, semester);
 
@@ -24,8 +51,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
     @Override
     /* Update enrolment of a student */
     public void updateEnrolment(String idOrName, String cidOrName, String semester, int option) {
-        ArrayList<StudentEnrolment> filteredList = studentListFilter(idOrName, semester);
-        ArrayList<Course> courseList = allCourses();
+        ArrayList<StudentEnrolment> filteredList = studentSemFilter(idOrName, semester);
         //Get student's info from the list
         String sid = filteredList.get(0).getStudent().getId();
         String sname = filteredList.get(0).getStudent().getName();
@@ -53,6 +79,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
                         Student student = new Student(sid, sname, birthdate);
                         Course course = new Course(cid, cname, credit_num);
                         enrollList.add(new StudentEnrolment(student, course, semester));
+                        System.out.println("The student is now added to the course.");
                         break;
                     }
                 }
@@ -71,6 +98,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
 
                         //remove if the enrolment's course matches the input course
                         enrollList.removeIf(s -> s.getCourse().getId() == cid);
+                        System.out.println("The student is now removed from the course.");
                         break;
                     }
                 }
@@ -107,7 +135,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
 
     /*** ADDITIONAL METHODS ***/
     /* Filter out an enrollment list of the REQUIRED STUDENT and SEMESTER*/
-    public ArrayList<StudentEnrolment> studentListFilter(String sidOrname, String semester) {
+    public ArrayList<StudentEnrolment> studentSemFilter(String sidOrname, String semester) {
         ArrayList<StudentEnrolment> filteredList = new ArrayList<>();
         //filter out a list according to student's name/id and semester.
         for (StudentEnrolment s : enrollList) {
@@ -121,7 +149,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
     }
 
     /* Filter out an enrollment list of the REQUIRED COURSES and SEMESTER*/
-    public ArrayList<StudentEnrolment> courseListFilter(String cidOrname, String semester) {
+    public ArrayList<StudentEnrolment> courseSemFilter(String cidOrname, String semester) {
         ArrayList<StudentEnrolment> filteredList = new ArrayList<>();
         //filter out a list according to student's name/id and semester.
         for (StudentEnrolment s : enrollList) {
@@ -134,7 +162,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
     }
 
     /* Filter out an enrollment list of the REQUIRED SEMESTER */
-    public ArrayList<StudentEnrolment> semesterListFilter(String semester) {
+    public ArrayList<StudentEnrolment> semFilter(String semester) {
         ArrayList<StudentEnrolment> filteredList = new ArrayList<>();
         //filter out a list according to student's name/id and semester.
         for (StudentEnrolment s : enrollList) {
@@ -145,9 +173,9 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
         return filteredList;
     }
 
-    /* Filter out all COURSES from Enrollment list */
-    public ArrayList<Course> allCourses() {
-        ArrayList<Course> courseList = new ArrayList<>();
+    /* Filter out all COURSES from Enrolment list */
+    public HashSet<Course> allCourses() {
+        HashSet<Course> courseList = new HashSet<>();
         //add course from 'enrollList' to 'courseList'
         for (StudentEnrolment s : enrollList) {
             courseList.add(s.getCourse());
@@ -155,9 +183,18 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
         return courseList;
     }
 
+    /* Filter out all STUDENTS from Enrolment list */
+    public ArrayList<Student> allStudents() {
+        ArrayList<Student> studentList = new ArrayList<>();
+        for (StudentEnrolment s : enrollList) {
+            studentList.add(s.getStudent());
+        }
+        return studentList;
+    }
+
     /* Print ALL COURSES for 1 STUDENT in 1 SEMESTER */
-    public void printCoursesOfStudent(String sidOrName, String semester) {
-        ArrayList<StudentEnrolment> filteredList = studentListFilter(sidOrName, semester);
+    public void printCoursesOfStudentSem(String sidOrName, String semester) {
+        ArrayList<StudentEnrolment> filteredList = studentSemFilter(sidOrName, semester);
         //Get student's info from the list
         String sid = filteredList.get(0).getStudent().getId();
         String sname = filteredList.get(0).getStudent().getName();
@@ -169,8 +206,8 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
     }
 
     /* Print ALL STUDENTS of 1 COURSES in 1 SEMESTER */
-    public void printStudentsOfCourse(String cidOrName, String semester) {
-        ArrayList<StudentEnrolment> filteredList = courseListFilter(cidOrName, semester);
+    public void printStudentsOfCourseSem(String cidOrName, String semester) {
+        ArrayList<StudentEnrolment> filteredList = courseSemFilter(cidOrName, semester);
         //Get course's info from the list
         String cid = filteredList.get(0).getCourse().getId();
         String cname = filteredList.get(0).getCourse().getName();
@@ -183,7 +220,7 @@ public class StudentEnrolmentList implements StudentEnrolmentManager {
 
     /* Print ALL COURSES in 1 SEMESTER */
     public void printCoursesOfSem(String semester) {
-        ArrayList<StudentEnrolment> filteredList = semesterListFilter(semester);
+        ArrayList<StudentEnrolment> filteredList = semFilter(semester);
         System.out.printf("%s 's Courses:", semester);
         //List all the Courses of the Semester
         for (StudentEnrolment s : filteredList) {
--- src/Enrollment/StudentEnrolmentManager.java
+++ src/Enrollment/StudentEnrolmentManager.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 
 public interface StudentEnrolmentManager {
     /* Add new enrolment */
-    public void addEnrolment(String sid, String name, String date, String cid, String cname, int credit_num, String semester);
+    public void addEnrolment(String sidOrName, String cidOrName, String semester);
 
     /* Update enrolment of a student */
     public void updateEnrolment(String idOrName, String cidOrName, String semester, int option);
